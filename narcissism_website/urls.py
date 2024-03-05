c3a1602c2abceb542cd88a0b6089d46e67a33e8b
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-05 22:06:03 +0000

    FEAT: Add homepage and about_page apps
    This commit introduces two new Django apps, 'homepage' and 'about_page', to the project. The 'homepage' app will handle the main landing page of the website, displaying main topics. The 'about_page' app will contain the About page, including a form for contact information.
    
    - Started testing functionality by setting up testing views.
    - Further development and testing are required.

--- narcissism_website/urls.py
+++ narcissism_website/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from homepage import views as index_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", index_views.index, name="index"),
+    path("admin/", admin.site.urls),
 ]
