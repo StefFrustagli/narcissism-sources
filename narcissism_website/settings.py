25dd84ec839b34b14753a9ccac0139b3d1fc7e1b
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-06 20:36:54 +0000

    CHORE: Set up project for deployment on Heroku
    This commit includes necessary configurations and adjustments to prepare the project for deployment on Heroku.
    
    Changes:
    - install gunicorn;
    - create Procfile;
    - create command to execute the project in Procfile;
    - update ALLOWED_HOSTS in settings with ,'.herokuapp.com'

--- narcissism_website/settings.py
+++ narcissism_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-()c-a!io@860op5ch++rw*th=e=p2+o(wxunu2--ic2boy*^-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
--- narcissism_website/urls.py
+++ narcissism_website/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from homepage import views as index_views
 
 urlpatterns = [
-    path("", index_views.index, name="index"),
+    path("homepage/", index_views.index, name="index"),
     path("admin/", admin.site.urls),
 ]
