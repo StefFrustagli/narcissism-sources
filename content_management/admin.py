bf164982c998bcff9782f73a2c19f3198af2e2e2
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-07 21:11:22 +0000

    CHORE: Add Content and Source models to content_management app
    This commit adds the Content and Source models to the content_management app. These models are essential for managing and organising content within the application.
    
    The Content model represents individual content items, such as articles, videos, or images, and includes fields for the title, description, embed code, and references to the source and topic of the content.
    
    The Source model represents the sources of the content, such as authors, publishers, or platforms, and includes fields for the name and link of the source, providing information about the origin of the content.

--- content_management/admin.py
+++ content_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Source
+from .models import Content
 
 # Register your models here.
+admin.site.register(Source)
+admin.site.register(Content)
--- content_management/models.py
+++ content_management/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from homepage.models import Topic
 
 # Create your models here.
+class Source(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.URLField()
+    slug = models.SlugField(unique=True, blank=True)
+
+class Content(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    embed_code = models.TextField()
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True)
