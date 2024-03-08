1a8fc1402f7bed7aff1373b382f78534867c03be
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-08 21:20:09 +0000

    FEAT: Add updated_on field to Comment model
    This commit introduces the `updated_on` field to the Comment model, allowing for tracking of the last update timestamp for comments.
    
    The `updated_on` field provides valuable information about when a comment was last modified, enhancing the functionality and usability of the application.

--- comments/models.py
+++ comments/models.py
@@ -17,6 +17,7 @@ class Comment(models.Model):
     )
     body = models.TextField(default="")
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=False)
 
     # class Meta:
--- content_management/admin.py
+++ content_management/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Source
-from .models import Content
+from .models import Source, Content
 
 # Register your models here.
 admin.site.register(Source)
