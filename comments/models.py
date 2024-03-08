14aa6397ffbae53a3a224c61466a85296f745687
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-08 23:17:16 +0000

    REFACTOR: Update Comment and Topic models
    Changes in the Comment model:
    - remove Source and Content fields as they are no needed at the moment;
    - add meta data and a __str__ method.
    
    Changes in the Topic model:
    - add meta data and a __str__ method.

--- comments/models.py
+++ comments/models.py
@@ -9,23 +9,23 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter", default=1
     )
-    content = models.ForeignKey(
-        Content, on_delete=models.CASCADE, related_name="comments", blank=True
-    )
-    source = models.ForeignKey(
-        Source,
-        on_delete=models.CASCADE,
-        related_name="comments",
-        default=None,
-        blank=True,
-    )
+    # content = models.ForeignKey(
+    #     Content, on_delete=models.CASCADE, related_name="comments", blank=True
+    # )
+    # source = models.ForeignKey(
+    #     Source,
+    #     on_delete=models.CASCADE,
+    #     related_name="comments",
+    #     default=None,
+    #     blank=True,
+    # )
     body = models.TextField(default="")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=False)
 
-    # class Meta:
-    #     ordering = ["created_on"]
+    class Meta:
+        ordering = ["created_on"]
 
-    # def __str__(self):
-    #     return f"Comment {self.body} by {self.author}"
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
--- homepage/models.py
+++ homepage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -10,3 +11,10 @@ class Topic(models.Model):
     description = models.TextField(blank=True)
     # page_url = models.URLField()  # Add this field to store the URL of the page
     status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Topic: {self.title}"
