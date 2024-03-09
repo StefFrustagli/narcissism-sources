0f93be8ad123f1b8450f770d8c623a82f5ae8128
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-09 23:17:51 +0000

    FEAT: Add template for homepage
    This commit adds a template from the 'Code Institute walkthrough Codestar blog' to the homepage templates directory. This template will serve as a placeholder for future development and will be used as a starting point for designing the homepage layout.

--- homepage/views.py
+++ homepage/views.py
@@ -6,4 +6,5 @@ from .models import Topic
 # Create your views here.
 class TopicList(generic.ListView):
     queryset = Topic.objects.all()
-    template_name = "topic_list.html"
+    template_name = "homepage/index.html"
+    paginate_by = 6
--- narcissism_website/settings.py
+++ narcissism_website/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -65,7 +65,7 @@ ROOT_URLCONF = "narcissism_website.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -132,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
