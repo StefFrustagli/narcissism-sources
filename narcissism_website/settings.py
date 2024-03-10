b5b0db6defb2fbf9382f92d23458c2f941548dd3
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-10 12:37:26 +0000

    CONFIG: Enable serving of static files
    This commit updates the project settings to enable serving of static files. Static files such as CSS, JavaScript, and images are now configured to be served correctly by the Django application. This ensures that the frontend of the application displays properly by serving the necessary static assets.
    
    Changes:
    - set up whitenoise~=5.3.0;
    - add STATIC_ROOT path;
    - create runtime.txt and add correct Python version for deployment.

--- narcissism_website/settings.py
+++ narcissism_website/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -135,6 +136,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
