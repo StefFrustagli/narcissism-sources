f7d344529e091c41bbceb05eddbb8e921a9d81ab
StefFrustagli <stefania.frustagli@outlook.it>
2024-03-18 22:07:45 +0000

    FEAT: Configure Cloudinary to manage image storage
    Change:
    - implement Cloudinary integration for better management of image uploads, storage, and delivery.

--- homepage/models.py
+++ homepage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -9,6 +10,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Topic(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     description = models.TextField(blank=True)
     # page_url = models.URLField()  # Add this field to store the URL of the page
     status = models.IntegerField(choices=STATUS, default=0)
--- narcissism_website/settings.py
+++ narcissism_website/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
     "django.contrib.sites",
     "allauth",
     "allauth.account",
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "django_summernote",
+    "cloudinary",
     "homepage",
     "about_page",
     "authentication",
